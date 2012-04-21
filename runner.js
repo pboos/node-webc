9f147297c3068241ca73ae467eb9cf4cbcd7d51d
Patrick Boos <patrickboos85@gmail.com>
2012-04-21 13:46:10 +0900

    Added logging and error handling for run() and serve() closes #5

--- runner.js
+++ runner.js
@@ -4,5 +4,7 @@ var dataPath = __dirname + '/test/_data';
 var outputPath = dataPath + '/_output';
 
 publisher.run({ path: dataPath, output: outputPath }, function(err, runner) {
+  if (err) { return console.log(err); }
+  
   console.log('Running...');
 });
--- serve.js
+++ serve.js
@@ -3,6 +3,8 @@ var publisher = require('./index');
 var dataPath = __dirname + '/test/_data';
 var outputPath = dataPath + '/_output';
 
-publisher.serve({ path: dataPath, output: outputPath, port: 8080 }, function(err, runner) {
+publisher.serve({ path: dataPath, output: outputPath, port: 8080 }, function(err) {
+  if (err) { return console.log(err); }
+
   console.log('Running on http://localhost:8080');
 });
